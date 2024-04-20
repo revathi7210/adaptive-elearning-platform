0093166a1c8b0e6e3644fc6a274956638510e974
Nikitha2404 <hebbar.nikitha@gmail.com>
2024-04-20 11:52:21 +0530

    adding signup

--- website/routes.py
+++ website/routes.py
@@ -1,8 +1,26 @@
-from flask import render_template,url_for,redirect,request,session
-from website import app
-import os
+from flask import render_template, url_for, redirect, request, session
+from website import app, db
+from website.models import User
 
+@app.route('/signup', methods=['POST'])
+def signup():
+    username = request.form['username']
+    password = request.form['password']
+    email = request.form['email']
 
-@app.route('/')
-def home():
+    existing_user = User.query.filter_by(username=username).first()
+    existing_email = User.query.filter_by(email=email).first()
+    if existing_user or existing_email:
+        return "Username or email already exists. Please choose another one."
+    
+    new_user = User(username=username, email=email,password=password)
+
+    db.session.add(new_user)
+    db.session.commit()
+    StudentId = new_user.id
+
+    return redirect(url_for('/home',StudentId=StudentId))
+
+@app.route('<int:StudentId>/home')
+def home(StudentId):
     return render_template('home.html')
